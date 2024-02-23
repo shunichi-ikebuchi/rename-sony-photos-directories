cd5623398556e6d8fb7a71b00ada3f9753ad6573
Shunichi Ikebuchi <shunichi@pigeonworks.net>
2024-02-23 23:27:33 +0900

    Update directory name check to "a7iii" and destination copy path

--- copy-and-delete.sh
+++ copy-and-delete.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eu
 
-if [ ! -d /Volumes/Contents/a73/ ]; then
+if [ ! -d /Volumes/Contents/a7iii/ ]; then
   echo "Drive is not mounted."
   exit 1
 fi
@@ -8,7 +8,7 @@ fi
 cp -rf /Volumes/1-1/DCIM/* ~/Pictures/tmp/
 cd ~/Pictures/tmp
 rename-sony-photos-directories
-cp -rf ~/Pictures/tmp/* /Volumes/Contents/a73/
+cp -rf ~/Pictures/tmp/* /Volumes/Contents/a7iii/
 rm -rf /Volumes/1-1/DCIM/*
 rm -rf ~/Pictures/tmp/*
 diskutil eject 1-1
